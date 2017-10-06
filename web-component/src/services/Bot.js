1af58c80fc02dfaab50edec72ca430ce106774a0
Guest <guest_@EXD0011.local>
2017-10-06 13:23:51 +0200

    Add a first draft of the bot

--- web-component/src/services/Bot.js
+++ web-component/src/services/Bot.js
@@ -1,5 +1,31 @@
 export default class Bot {
-  answerQuestion(question) {
-    return `You asked me: "${question}"... But I have no clue what to answer!`
+  answerQuestion(input) {
+    var lowerCaseInput = input.toLowerCase()
+
+    switch (true) {
+      case lowerCaseInput.startsWith('what'):
+        return this.matchWhatInput(lowerCaseInput)
+      case lowerCaseInput.startsWith('how'):
+        return this.matchHowInput(lowerCaseInput)
+    }
+  }
+
+  matchWhatInput(input) {
+    switch (true) {
+      case /.*quality gate.*/.test(input):
+        return {
+          msg: 'A quality gate is the best way to enforce a quality policy in your organization.',
+          url: 'https://docs.sonarqube.org/display/SONAR/Quality+Gates'
+        }
+      case /.*quality profile.*/.test(input):
+        return {
+          msg: 'The Quality Profiles service is central to SonarQube, since it is where you define your requirements by defining sets of rules',
+          url: 'https://docs.sonarqube.org/display/SONAR/Quality+Profiles'
+        }
+    }
+  }
+
+  matchHowInput(input) {
+    return ''
   }
 }
